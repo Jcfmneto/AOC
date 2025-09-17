002d51fde76d0d608e5becbe56dc087bd99d73f6
Julio <jcfmneto@gmail.com>
2025-09-17 16:24:31 -0300

    First Start -- Day 1

---
+++ 23/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fileContent := string(readFile("input.txt"))
+
+	lines := strings.Split(fileContent, "\n")
+	fmt.Println(returnPoints(lines))
+}
+
+func readFile(inputPath string) []byte {
+	dat, _ := os.ReadFile(inputPath)
+
+	return dat
+}
+
+func returnPoints(lines []string) int {
+
+	var totalSum int
+
+	for _, line := range lines {
+
+		if len(line) == 0 {
+			continue
+		}
+
+		var firstDigit rune
+		var lastDigit rune
+
+		for _, char := range line {
+			if isDigit(char) {
+				firstDigit = char
+				break
+			}
+		}
+		for i := len(line) - 1; i >= 0; i-- {
+			char := rune(line[i])
+
+			if isDigit(char) {
+				lastDigit = char
+				break
+			}
+		}
+		number := int(firstDigit-'0')*10 + int(lastDigit-'0')
+		totalSum += number
+
+	}
+
+	return totalSum
+
+}
+
+func isDigit(r rune) bool {
+	return r >= '0' && r <= '9'
+}
